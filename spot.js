3239de40b6c5311bafc5765b068d56ea6de1d689
xushunke <xushunke>
2018-08-30 12:35:16 +0800

    first commit

--- spot.js
+++ spot.js
@@ -8,20 +8,35 @@
 
     start();
 //    document.getElementById('go').addEventListener('click', start);
-    
+
+
+
     function start() {
 //        document.getElementById('go').removeEventListener('click', start);
-        promise.then(() => engine.toText('Oops'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('Xsk'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('Love'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('Yt'))
-        .then(() => engine.shake())
-        .then(() => engine.clear())
-        .then(() => {}
-            // document.getElementById('go').addEventListener('click', start)
-        );
+
+        var txtQuery = getQueryVariable("text");
+        if(!txtQuery){
+            txtQuery = "Oops,Xsk,Love,Yt"
+        }
+        var txtArr = txtQuery.split(',');
+        for(var i in txtArr){
+            let txtArrElement = txtArr[i];
+            promise = promise
+                .then(() => engine.toText(txtArrElement))
+                .then(() => engine.shake());
+        }
+        promise
+            .then(() => engine.clear())
+    }
+
+    function getQueryVariable(variable)
+    {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+        }
+        return(false);
     }
 })();
